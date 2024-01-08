586f30eaf1ffe87a0e3be3f09ff9ec39c2867c70
Say my name <kowalsan4ik@gmail.com>
2024-01-08 19:39:28 +0200

    Initial commit, already created Cart, Order and Product entities

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Order {
+    private int orderId;
+    private List<Product> products;
+    private String status;
+
+    public Order(int orderId, List<Product> products, String status) {
+        this.orderId = orderId;
+        this.products = products;
+        this.status = status;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
