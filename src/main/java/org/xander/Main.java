d72b8502964cf500c80b4a4efa3aec0f115a83d3
Say my name <kowalsan4ik@gmail.com>
2024-01-08 21:02:30 +0200

    E-commerce actions through Main

--- src/main/java/org/xander/Main.java
+++ src/main/java/org/xander/Main.java
@@ -1,19 +1,54 @@
 package org.xander;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Product laptop = new Product(1, "Acer Aspire X", 50000.1);
+        Product smartphone = new Product(2, "YPhone", 10800.0);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Cart cart = new Cart();
+
+        cart.addProduct(laptop);
+        cart.addProduct(smartphone);
+
+        System.out.println("Кошик після додавання продуктів:");
+        displayCart(cart);
+
+        cart.removeProduct(laptop);
+
+        System.out.println("Кошик після видалення продукту:");
+        displayCart(cart);
+
+        Order order = placeOrder(cart);
+
+        System.out.println("Статус замовлення: " + order.getStatus());
+    }
+
+    private static void displayCart(Cart cart) {
+        List<Product> products = cart.getProducts();
+        for (Product product : products) {
+            System.out.println("ID: " + product.getId() +
+                    ", Назва: " + product.getName() +
+                    ", Ціна: " + product.getPrice());
         }
+        System.out.println("Загальна вартість: " + calculateTotalPrice(cart) + "\n");
+    }
+
+    private static double calculateTotalPrice(Cart cart) {
+        return cart.getProducts().stream()
+                .mapToDouble(Product::getPrice)
+                .sum();
+    }
+
+    private static Order placeOrder(Cart cart) {
+
+        int orderId = 1;
+        String orderStatus = "Pending...";
+        List<Product> orderedProducts = cart.getProducts();
+
+        return new Order(orderId, orderedProducts, orderStatus);
     }
 }
